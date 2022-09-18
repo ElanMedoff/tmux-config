5f3fb78f3db98be0ae52d7964eabf37073e8c362
Elan <54599408+ElanMedoff@users.noreply.github.com>
2022-09-17 23:46:25 -0400

    rename coloredEcho to cecho

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
-function coloredEcho(){
+function cecho(){
     tput setaf $2;
     echo $1;
     tput sgr0;
 }
 
 if [ "$(uname)" != "Darwin" ]; then
-  coloredEcho "sorry! this script only supports macos" 1
+  cecho "sorry! this script only supports macos" 1
   exit 1
 fi
 
 if [ "$(command -v brew)" == "" ]; then
-    coloredEcho "installing hombrew" 2
+    cecho "installing hombrew" 2
     NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-  coloredEcho "homebrew already installed" 4
+  cecho "homebrew already installed" 4
 fi
 
 if [ "$(brew ls --versions tmux)" == "" ]; then
-  coloredEcho "installing tmux" 2
+  cecho "installing tmux" 2
   brew install tmux
 else
-  coloredEcho "tmux already installed" 4
+  cecho "tmux already installed" 4
 fi
 
 tpm_directory="$HOME/.config/tmux/plugins/tpm"
 if [ ! -d "$tpm_directory" ]; then
-  coloredEcho "installing tpm" 2
+  cecho "installing tpm" 2
   git clone https://github.com/tmux-plugins/tpm $tpm_directory
   "$tpm_directory/bin/install_plugins"
 else
-  coloredEcho "packer already installed" 4
+  cecho "packer already installed" 4
 fi
