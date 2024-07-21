1afb5a913b7dca928008c97d11a5b7d4e24a3754
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-07-21 00:01:38 -0400

    convert deno scripts to bash

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+source ~/.dotfiles/helpers.sh
+
+h_validate_num_args --num=1 "$@"
+h_validate_package_manager $1
+
+h_install_package $1 tmux
+
+if [[ -d ~/.tmux/plugins/tpm/ ]]
+then
+  h_cecho --noop "tpm already installed"
+else
+  h_cecho --doing "cloning tpm"
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm/
+  h_cecho --doing "installing tpm plugins"
+  ~/.tmux/plugins/tpm/bin/install_plugins
+fi
