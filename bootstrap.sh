88c0dcf60c160b398e469c7a8b6d0b3b58b01017
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-10-05 21:45:06 -0400

    bootstrap cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,17 +3,17 @@
 source ~/.dotfiles/helpers.sh
 
 h_validate_num_args --num=1 "$@"
-h_validate_package_manager $1
+h_validate_package_manager "$1"
 
-h_install_package $1 tmux
+h_install_package "$1" "tmux"
 
 if [[ -d ~/.tmux/plugins/tpm/ ]]
 then
-  h_cecho --noop "tpm already installed"
+  h_echo --mode=noop "tpm already installed"
 else
-  h_cecho --doing "cloning tpm"
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm/
+  h_echo --mode=doing "cloning tpm"
+  git clone "https://github.com/tmux-plugins/tpm" "~/.tmux/plugins/tpm/"
 fi
 
-h_cecho --doing "installing tpm plugins"
+h_echo --mode=doing "installing tpm plugins"
 ~/.tmux/plugins/tpm/bin/install_plugins
