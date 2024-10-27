c44244359c4d29a635c2a0cacc215d558197a061
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-10-26 17:09:20 -0700

    move to ruby bootstrap

---
+++ bootstrap.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require_relative File.expand_path '~/.dotfiles/helpers.rb'
+
+package_manager = get_package_manager_arg
+validate_package_manager package_manager
+
+install_package package_manager, 'tmux'
+
+tpm_dir = File.expand_path '~/.tmux/plugins/tpm'
+if Dir.exist? tpm_dir
+  puts 'tpm already installed'.noop
+else
+  puts 'cloning tpm'.doing
+  `git clone "https://github.com/tmux-plugins/tpm" #{tpm_dir}`
+end
+
+puts 'installing tpm plugins'.doing
+stream_command '~/.tmux/plugins/tpm/bin/install_plugins'
